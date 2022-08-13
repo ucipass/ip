13674f4347770a351ccde54590d87a4258985d25
ucipass@gmail.com <ucipass@gmail.com>
2022-08-13 12:52:03 -0500

    tcp event handlers

--- telnet.js
+++ telnet.js
@@ -3,12 +3,31 @@ const log = require('./log.js')
 const geolookup = require('./geolookup.js')
 const TELNET_PORT = process.env.TELNET_PORT || "2323"
 
-const server = net.createServer( async (tcpsocket) => {
-    let ipaddr = tcpsocket.remoteAddress
+const server = net.createServer( async (tcp) => {
+    let ipaddr = tcp.remoteAddress
+    tcp.on("close",(err)=>{
+        log.debug(`tcp connection closed for ${ipaddr}!`)  
+    })
+    tcp.on("data", (data) =>{
+        log.silly(`tcp data received from ${ipaddr}!`)  
+    })
+    tcp.on("drain", () =>{
+        log.silly(`tcp write buffer becomes empty for ${ipaddr}!`)  
+    })
+    tcp.on("error", (err) =>{
+        log.silly(`tcp error occured for ${ipaddr}!`)  
+    })
+    tcp.on("ready", () =>{
+        log.silly(`tcp socket is ready for ${ipaddr}!`)  
+    })
+    tcp.on("timeout", () =>{
+        log.silly(`tcp timeout occured for ${ipaddr}!`)  
+    })
+
     let output = await geolookup(ipaddr,"Telnet Client")
     try {
-        tcpsocket.write(output)
-        tcpsocket.end()
+        tcp.write(output)
+        tcp.end()
     } catch (error) {
         log.error(`Exception for: ${ipaddr}`)
         log.debug(output)
@@ -20,6 +39,9 @@ server.maxConnections = 10
 server.listen(TELNET_PORT, '0.0.0.0',()=> { 
     log.info(`Telnet server started on port ${TELNET_PORT}!`)  
 })
+server.on('close', (err) => {
+    log.error("Telnet closed with all connections ended!")
+});
 server.on('error', (err) => {
     log.error("Telnet Server Error!")
     log.debug(err)

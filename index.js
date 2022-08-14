d5a8e6d055b9a4dc1d36bf489779f9c320173b20
ucipass@gmail.com <ucipass@gmail.com>
2022-08-13 20:05:30 -0500

    cipher change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http   = require("./telnet.js")
-const telnet = require("./http.js")
-const syslog = require("./syslog.js")
+// const http   = require("./telnet.js")
+// const telnet = require("./http.js")
+// const syslog = require("./syslog.js")
 const ssh    = require("./ssh.js")

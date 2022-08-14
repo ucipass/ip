422275aefdb22a129e6475aca5a39527b4aafebd
ucipass@gmail.com <ucipass@gmail.com>
2022-08-13 20:10:11 -0500

    cipher change2

--- ssh.js
+++ ssh.js
@@ -7,9 +7,9 @@ const SSH_PORT = process.env.SSH_PORT || "2222"
 const server = new Server({
   hostKeys: [readFileSync('host.key')],
   algorithms:  {
-    cipher: ["aes128-gcm","aes128-gcm@openssh.com","aes256-gcm","aes256-gcm@openssh.com","aes128-ctr","aes192-ctr","aes256-ctr","3des-cbc","aes256-cbc","aes192-cbc","aes128-cbc","arcfour256","arcfour128","arcfour","blowfish-cbc","cast128-cbc"],
+    cipher: ["aes128-gcm","aes128-gcm@openssh.com","aes256-gcm","aes256-gcm@openssh.com","aes128-ctr","aes192-ctr","aes256-ctr","3des-cbc","aes256-cbc","aes192-cbc","aes128-cbc"],
     compress: ["none"],
-    hmac: ["hmac-sha2-256-etm@openssh.com","hmac-sha2-512-etm@openssh.com","hmac-sha1-etm@openssh.com","hmac-sha2-256","hmac-sha2-512","hmac-sha1","hmac-md5","hmac-sha2-256-96","hmac-sha2-512-96","hmac-ripemd160","hmac-sha1-96","hmac-md5-96"],
+    hmac: ["hmac-sha2-256-etm@openssh.com","hmac-sha2-512-etm@openssh.com","hmac-sha1-etm@openssh.com","hmac-sha2-256","hmac-sha2-512","hmac-sha1","hmac-md5","hmac-sha2-256-96","hmac-sha2-512-96","hmac-sha1-96","hmac-md5-96"],
     kex: ["curve25519-sha256","curve25519-sha256@libssh.org","ecdh-sha2-nistp256","ecdh-sha2-nistp384","ecdh-sha2-nistp521","diffie-hellman-group-exchange-sha256","diffie-hellman-group14-sha256","diffie-hellman-group15-sha512","diffie-hellman-group16-sha512","diffie-hellman-group17-sha512","diffie-hellman-group18-sha512","diffie-hellman-group-exchange-sha1","diffie-hellman-group14-sha1","diffie-hellman-group1-sha1" ]
   }
 }, (client) => {

3a06c8bb91ffc1da1eba37da988bb2c71537f8f1
ucipass@gmail.com <ucipass@gmail.com>
2022-08-26 10:33:48 -0500

    uptime

--- sio.js
+++ sio.js
@@ -2,7 +2,11 @@ const sio = require("socket.io");
 const http = require("./http.js")
 const geolookup = require('./geolookup.js')
 const log = require("./log.js")
+const dayjs = require('dayjs')
+const relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
 
+const server_start = dayjs()
 // Exports Socket.io instance
 module.exports = http
 .then( server => {
@@ -17,7 +21,8 @@ module.exports = http
         // let headers = socket.handshake.headers
         let ipaddr = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address
         const status = {
-            connections: io.sockets.sockets.size
+            connections: io.sockets.sockets.size,
+            server_start: server_start.from(dayjs())
         } 
         socket.emit("status",status);
         let output = await geolookup(ipaddr,"HTTP Client")
